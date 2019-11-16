57f89001814ca2911c03639db9845814d98f76b0
chad2wick <chad.chadwick@blockworks.com>
2019-11-16 08:41:59 -0500

    recompiled typescript

--- dist/WIDetailsWidget.js
+++ dist/WIDetailsWidget.js
@@ -1,1 +1,227 @@
-define(["TFS/Work/RestClient","TFS/WorkItemTracking/RestClient","TFS/WorkItemTracking/Services"],function(e,t,n){return function(e){function t(o){if(n[o])return n[o].exports;var i=n[o]={exports:{},id:o,loaded:!1};return e[o].call(i.exports,i,i.exports,t),i.loaded=!0,i.exports}var n={};return t.m=e,t.c=n,t.p="",t(0)}([function(e,t,n){var o,i;o=[n,t,n(4),n(5),n(7),n(2),n(3)],i=function(e,t,n,o,i,r,a){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var s=function(){function e(e){this.WidgetHelpers=e,this.client=n.getClient(),this.clientwi=o.getClient()}return e.prototype.LoadWIDetails=function(e){var t=this;r.TelemetryClient.getClient(a.settings).trackPageView("Index");var n=JSON.parse(e.customSettings.data),o=$("h2");return o.text(e.name),n?($("#configwidget").hide(),$("#loadingwidget").show(),$("#content").hide(),$("#contentError").hide(),this.clientwi.getWorkItem(n.wiId).then(function(e){console.log("WorkItemDetailWidget:LoadWIDetails step 1"),console.log("WorkItemDetailWidget:LoadWIDetails propertyName: "+n.wiPropertyName+"; color prop: "+n.wiColorPropertyName+"; color: "+n.color+"; title: "+n.title),t.DisplayWIDetails(e,n.wiPropertyName,n.wiColorPropertyName,n.color,n.title),$("#loadingwidget").hide(),$("#content").show(),$("#wi-title").dotdotdot(),$("#wi-desc").dotdotdot(),$(".widget").off(),$(".widget").on("click",function(){i.WorkItemFormNavigationService.getService().then(function(e){e.openWorkItem(n.wiId,!1)})})},function(e){$("#loadingwidget").hide(),$("#contentError").attr("style","display:block;margin:10px;"),$("#contentError").addClass("error"),"404"===e.status?$("#contentError").html("TF401232: Work item Id: "+n.wiId+" does not exist, or you do not have permissions to read it."):$("#contentError").html(e.message),r.TelemetryClient.getClient(a.settings).trackException(e.message)})):(o.attr("style","color:grey"),$("#content").hide(),$("#loadingwidget").hide(),$("#contentError").hide(),$("#configwidget").show()),this.WidgetHelpers.WidgetStatusHelper.Success()},e.prototype.DisplayWIDetails=function(e,t,n,o,i){$("#wi-header").attr("style","height: 30px;"),$("#wi-type").attr("style","height: 10px;"),$("#updated").attr("style","height: 0px;"),$("#assignuser").attr("style","height: 0px;"),$("#statecircle").attr("style","height: 0px;"),$("#wi-desc").attr("style","height: calc(100vh - 30px); min-height: calc(100vh - 30px);");var r=e.fields[n],a=e.fields["System.Title"],s=t.split(".");a=s.pop(),a=i;var c=this.getWorkItemColor(r);""!==o&&($("#content").attr("style","background-color: "+o+";"),c=o,console.log("WorkItemDetailWidget:DisplayWIDetails color prop: "+n+"; value: "+c+"; color: "+o)),$("#wi-header").attr("style","background-color: "+c+";"),$("#wi-title").text(a);var l=e.fields[t];void 0!==l?$("#wi-desc").html(l):$("#wi-desc").html(""),$("#updateby").html("Updated by ".concat(e.fields["System.ChangedBy"])),$("#updatedate").html(this.DeltaDate(new Date(e.fields["System.ChangedDate"])).text);var d=e.fields["System.AssignedTo"];void 0!==d?($("#assignuser").html(d),$("#assignavatar").attr("src",this.getMemberAvatarUrl(d))):($("#assignuser").html("Unassigned"),$("#assignavatar").hide());var p=e.fields["System.State"];$("#state").html(p);var g=this.getStateColor(p),u=g;"Removed"===p&&(u="transparent"),$("#statecircle").attr("style","border-color:"+g+";background-color:"+u)},e.prototype.noHtml=function(e){var t=e.indexOf("<"),n=e.indexOf(">"),o=e.length,i=e.substring(0,t);n===-1&&(n=t);var r=e.substring(n+1,o);return e=i+r,t!==n&&(e=this.noHtml(e)),e},e.prototype.getWorkItemColor=function(e){var t="";switch(e){case"AMBER":t="#ffbf00";break;default:t=e}return console.log("WorkItemDetailWidget:getWorkItemColor result: "+t),t},e.prototype.getStateColor=function(e){var t="";switch(e){case"Approved":case"New":case"To Do":case"Design":t="#b2b2b2";break;case"In Progress":case"Committed":case"Open":case"Ready":case"Active":case"In Planning":t="#007acc";break;case"Done":case"Closed":case"Inactive":case"Completed":t="#393";break;case"Removed":t="#5688e0";break;default:t="#b2b2b2"}return t},e.prototype.DeltaDate=function(e){var t=Date.now(),n=e.getTime(),o=Math.floor((t-n)/864e5),i={days:o,text:""};if(o>365){var r=Math.floor(o/365);1===r?i.text="A year ago":i.text=r+" year(s) ago"}else if(o>30){var a=Math.floor(o/30);1===a?i.text="A month ago":i.text=a+" month(s) ago"}else o>7?i.text="This month":o<=7&&o>1?i.text=o+" days ago":1===o?i.text="Yesterday":i.text="Today";return i},e.prototype.getMemberAvatarUrl=function(e){var t=e.lastIndexOf("<"),n=e.lastIndexOf(">"),o=$.trim(e.substr(t+1,n-t-1));return VSS.getWebContext().host.uri+"_api/_common/IdentityImage?id=&identifier="+o},e.prototype.load=function(e){return this.LoadWIDetails(e)},e.prototype.reload=function(e){return this.LoadWIDetails(e)},e}();t.WidgetWIDetails=s,VSS.require("TFS/Dashboards/WidgetHelpers",function(e){e.IncludeWidgetStyles(),VSS.register("widetailswidget",function(){var t=new s(e);return t}),VSS.notifyLoadSucceeded()})}.apply(t,o),!(void 0!==i&&(e.exports=i))},function(e,t,n){var o,i;o=[n,t],i=function(e,t){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var n=function(){function e(){}return e._createLazyMethod=function(t){var n=window[e.appInsightsName];n[t]=function(){var e=arguments;n.queue?n.queue.push(function(){return n[t].apply(n,e)}):n[t].apply(n,e)}},e._defineLazyMethods=function(){var t=window[e.appInsightsName];try{t.cookie=document.cookie}catch(e){}t.queue=[];for(var n=["clearAuthenticatedUserContext","flush","setAuthenticatedUserContext","startTrackEvent","startTrackPage","stopTrackEvent","stopTrackPage","trackDependency","trackEvent","trackException","trackMetric","trackPageView","trackTrace"];n.length;)e._createLazyMethod(n.pop())},e._download=function(t){e.appInsightsInstance.config=t;var n=window[e.appInsightsName];if(n.queue||(n.queue=[]),setTimeout(function(){var e=document.createElement("script");e.src=t.url||"https://az416426.vo.msecnd.net/scripts/a/ai.0.js",document.head.appendChild(e)}),!t.disableExceptionTracking){var o="onerror";e._createLazyMethod("_"+o);var i=window[o];window[o]=function(e,t,r,a,s){var c=i&&i(e,t,r,a,s);return c!==!0&&n["_"+o](e,t,r,a,s),c}}},Object.defineProperty(e,"appInsightsInstance",{get:function(){if("undefined"!=typeof window)return window[e.appInsightsName]||(window[e.appInsightsName]={downloadAndSetup:e._download,_defineLazyMethods:e._defineLazyMethods},e._defineLazyMethods()),window[e.appInsightsName]},enumerable:!0,configurable:!0}),e}();n.appInsightsInitialized=!1,n.appInsightsName="appInsights",t.AppInsights=n.appInsightsInstance}.apply(t,o),!(void 0!==i&&(e.exports=i))},function(e,t,n){var o,i;o=[n,t,n(1)],i=function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var o=function(){function e(){this.disableTelemetry="false",this.disableAjaxTracking="false",this.enableDebug="false"}return e}();t.TelemetryClientSettings=o;var i=function(){function e(){this.IsAvailable=!0}return e.getClient=function(t){return this._instance||(console.log("Creating new TelemetryClient!"),this._instance=new e,this._instance.Init(t)),this._instance},e.prototype.Init=function(e){console.log("TelemetryClient settings key: "+e.key.substring(0,8)+"************"),console.log("TelemetryClient settings extension context: "+e.extensioncontext),console.log("TelemetryClient settings disableTelemetry: "+("true"===e.disableTelemetry)),console.log("TelemetryClient settings disableAjaxTracking: "+("true"===e.disableAjaxTracking)),console.log("TelemetryClient settings enableDebug: "+("true"===e.enableDebug));var t={instrumentationKey:e.key,disableTelemetry:"true"===e.disableTelemetry,disableAjaxTracking:"true"===e.disableAjaxTracking,enableDebug:"true"===e.enableDebug};this.ExtensionContext=e.extensioncontext;try{var o=VSS.getWebContext();n.AppInsights.downloadAndSetup(t),n.AppInsights.setAuthenticatedUserContext(o.user.id,o.collection.id)}catch(e){console.log(e)}},e.prototype.trackPageView=function(e,t,o,i,r){try{n.AppInsights.trackPageView(this.ExtensionContext+"."+e,t,o,i,r),n.AppInsights.flush()}catch(e){console.log(e)}},e.prototype.trackEvent=function(e,t,o){try{console.log("Tracking event: "+this.ExtensionContext+"."+e),n.AppInsights.trackEvent(this.ExtensionContext+"."+e,t,o),n.AppInsights.flush()}catch(e){console.log(e)}},e.prototype.trackException=function(e,t,o,i){try{console.error(e);var r={name:this.ExtensionContext+"."+t,message:e};n.AppInsights.trackException(r,t,o,i),n.AppInsights.flush()}catch(e){console.log(e)}},e.prototype.trackMetric=function(e,t,o,i,r,a){try{n.AppInsights.trackMetric(this.ExtensionContext+"."+e,t,o,i,r,a),n.AppInsights.flush()}catch(e){console.log(e)}},e}();t.TelemetryClient=i}.apply(t,o),!(void 0!==i&&(e.exports=i))},function(e,t,n){var o,i;o=[n,t],i=function(e,t){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.settings={key:"__INSTRUMENTATIONKEY__",extensioncontext:"WorkItemDetailsWidget",disableTelemetry:"true",disableAjaxTracking:"__disableAjaxTracking__",enableDebug:"__enableDebug__"}}.apply(t,o),!(void 0!==i&&(e.exports=i))},function(t,n){t.exports=e},function(e,n){e.exports=t},,function(e,t){e.exports=n}])});
+define(["require", "exports", "TFS/Work/RestClient", "TFS/WorkItemTracking/RestClient", "TFS/WorkItemTracking/Services", "telemetryclient-team-services-extension", "./telemetryClientSettings"], function (require, exports, RestClient, RestClientWI, WorkItemServices, tc, telemetryClientSettings) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var WidgetWIDetails = (function () {
+        function WidgetWIDetails(WidgetHelpers) {
+            this.WidgetHelpers = WidgetHelpers;
+            this.client = RestClient.getClient();
+            this.clientwi = RestClientWI.getClient();
+        }
+        WidgetWIDetails.prototype.LoadWIDetails = function (widgetSettings) {
+            var _this = this;
+            tc.TelemetryClient.getClient(telemetryClientSettings.settings).trackPageView("Index");
+            var customSettings = JSON.parse(widgetSettings.customSettings.data);
+            var $title = $("h2");
+            $title.text(widgetSettings.name);
+            if (customSettings) {
+                $("#configwidget").hide();
+                $("#loadingwidget").show();
+                $("#content").hide();
+                $("#contentError").hide();
+                this.clientwi.getWorkItem(customSettings.wiId).then(function (wi) {
+                    console.log("WorkItemDetailWidget:LoadWIDetails step 1");
+                    console.log("WorkItemDetailWidget:LoadWIDetails propertyName: " + customSettings.wiPropertyName
+                        + "; color prop: " + customSettings.wiColorPropertyName + "; color: " + customSettings.color
+                        + "; title: " + customSettings.title);
+                    _this.DisplayWIDetails(wi, customSettings.wiPropertyName, customSettings.wiColorPropertyName, customSettings.color, customSettings.title);
+                    $("#loadingwidget").hide();
+                    $("#content").show();
+                    $("#wi-title").dotdotdot();
+                    $("#wi-desc").dotdotdot();
+                    $(".widget").off();
+                    $(".widget").on("click", function () {
+                        WorkItemServices.WorkItemFormNavigationService.getService().then(function (service) {
+                            service.openWorkItem(customSettings.wiId, false);
+                        });
+                    });
+                }, function (reject) {
+                    $("#loadingwidget").hide();
+                    $("#contentError").attr("style", "display:block;margin:10px;");
+                    $("#contentError").addClass("error");
+                    if (reject.status === "404") {
+                        $("#contentError").html("TF401232: Work item Id: " + customSettings.wiId + " does not exist, or you do not have permissions to read it.");
+                    }
+                    else {
+                        $("#contentError").html(reject.message);
+                    }
+                    tc.TelemetryClient.getClient(telemetryClientSettings.settings).trackException(reject.message);
+                });
+            }
+            else {
+                $title.attr("style", "color:grey");
+                $("#content").hide();
+                $("#loadingwidget").hide();
+                $("#contentError").hide();
+                $("#configwidget").show();
+            }
+            return this.WidgetHelpers.WidgetStatusHelper.Success();
+        };
+        WidgetWIDetails.prototype.DisplayWIDetails = function (wi, fieldname, colorfield, colorstring, titlestring) {
+            $("#wi-header").attr("style", "height: " + 30 + "px;");
+            $("#wi-type").attr("style", "height: " + 10 + "px;");
+            $("#updated").attr("style", "height: " + 0 + "px;");
+            $("#assignuser").attr("style", "height: " + 0 + "px;");
+            $("#statecircle").attr("style", "height: " + 0 + "px;");
+            $("#wi-desc").attr("style", "height: calc(100vh - " + 30 + "px); min-height: calc(100vh - " + 30 + "px);");
+            var witype = wi.fields[colorfield];
+            var wititle = wi.fields["System.Title"];
+            var stringParts = fieldname.split(".");
+            wititle = stringParts.pop();
+            wititle = titlestring;
+            var color = this.getWorkItemColor(witype);
+            if (colorstring !== "") {
+                $("#content").attr("style", "background-color: " + colorstring + ";");
+                color = colorstring;
+                console.log("WorkItemDetailWidget:DisplayWIDetails color prop: " + colorfield + "; value: " + color + "; color: " + colorstring);
+            }
+            $("#wi-header").attr("style", "background-color: " + color + ";");
+            $("#wi-title").text(wititle);
+            var desc = wi.fields[fieldname];
+            if (desc !== undefined) {
+                $("#wi-desc").html(desc);
+            }
+            else {
+                $("#wi-desc").html("");
+            }
+            $("#updateby").html("Updated by ".concat(wi.fields["System.ChangedBy"]));
+            $("#updatedate").html(this.DeltaDate(new Date(wi.fields["System.ChangedDate"])).text);
+            var assign = wi.fields["System.AssignedTo"];
+            if (assign !== undefined) {
+                $("#assignuser").html(assign);
+                $("#assignavatar").attr("src", this.getMemberAvatarUrl(assign));
+            }
+            else {
+                $("#assignuser").html("Unassigned");
+                $("#assignavatar").hide();
+            }
+            var state = wi.fields["System.State"];
+            $("#state").html(state);
+            var statecolor = this.getStateColor(state);
+            var backgroundcolor = statecolor;
+            if (state === "Removed") {
+                backgroundcolor = "transparent";
+            }
+            $("#statecircle").attr("style", "border-color:" + statecolor + ";background-color:" + backgroundcolor + "");
+        };
+        WidgetWIDetails.prototype.noHtml = function (txt) {
+            var a = txt.indexOf("<");
+            var b = txt.indexOf(">");
+            var len = txt.length;
+            var c = txt.substring(0, a);
+            if (b === -1) {
+                b = a;
+            }
+            var d = txt.substring((b + 1), len);
+            txt = c + d;
+            if (a !== b) {
+                txt = this.noHtml(txt);
+            }
+            return (txt);
+        };
+        WidgetWIDetails.prototype.getWorkItemColor = function (workItemType) {
+            var witColor = "";
+            switch (workItemType) {
+                case "AMBER":
+                    witColor = "#ffbf00";
+                    break;
+                default:
+                    witColor = workItemType;
+            }
+            console.log("WorkItemDetailWidget:getWorkItemColor result: " + witColor);
+            return witColor;
+        };
+        WidgetWIDetails.prototype.getStateColor = function (state) {
+            var statecolor = "";
+            switch (state) {
+                case "Approved":
+                case "New":
+                case "To Do":
+                case "Design":
+                    statecolor = "#b2b2b2";
+                    break;
+                case "In Progress":
+                case "Committed":
+                case "Open":
+                case "Ready":
+                case "Active":
+                case "In Planning":
+                    statecolor = "#007acc";
+                    break;
+                case "Done":
+                case "Closed":
+                case "Inactive":
+                case "Completed":
+                    statecolor = "#393";
+                    break;
+                case "Removed":
+                    statecolor = "#5688e0";
+                    break;
+                default:
+                    statecolor = "#b2b2b2";
+            }
+            return statecolor;
+        };
+        WidgetWIDetails.prototype.DeltaDate = function (date) {
+            var now = Date.now();
+            var past = date.getTime();
+            var days = Math.floor((now - past) / (1000 * 60 * 60 * 24));
+            var result = {
+                days: days,
+                text: ""
+            };
+            if (days > 365) {
+                var years = Math.floor(days / 365);
+                if (years === 1) {
+                    result.text = "A year ago";
+                }
+                else {
+                    result.text = years + " year(s) ago";
+                }
+            }
+            else if (days > 30) {
+                var months = Math.floor(days / 30);
+                if (months === 1) {
+                    result.text = "A month ago";
+                }
+                else {
+                    result.text = months + " month(s) ago";
+                }
+            }
+            else if (days > 7) {
+                result.text = "This month";
+            }
+            else if (days <= 7 && days > 1) {
+                result.text = days + " days ago";
+            }
+            else if (days === 1) {
+                result.text = "Yesterday";
+            }
+            else {
+                result.text = "Today";
+            }
+            return result;
+        };
+        WidgetWIDetails.prototype.getMemberAvatarUrl = function (memberIdentity) {
+            var i = memberIdentity.lastIndexOf("<");
+            var j = memberIdentity.lastIndexOf(">");
+            var uniqueName = $.trim(memberIdentity.substr(i + 1, j - i - 1));
+            return VSS.getWebContext().host.uri + "_api/_common/IdentityImage?id=&identifier=" + uniqueName;
+        };
+        WidgetWIDetails.prototype.load = function (widgetSettings) {
+            return this.LoadWIDetails(widgetSettings);
+        };
+        WidgetWIDetails.prototype.reload = function (widgetSettings) {
+            return this.LoadWIDetails(widgetSettings);
+        };
+        return WidgetWIDetails;
+    }());
+    exports.WidgetWIDetails = WidgetWIDetails;
+    VSS.require("TFS/Dashboards/WidgetHelpers", function (WidgetHelpers) {
+        WidgetHelpers.IncludeWidgetStyles();
+        VSS.register("widetailswidget", function () {
+            var widgetDetails = new WidgetWIDetails(WidgetHelpers);
+            return widgetDetails;
+        });
+        VSS.notifyLoadSucceeded();
+    });
+});
---
+++ dist/telemetryClientSettings.js
@@ -0,0 +1,11 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.settings = {
+        key: "__INSTRUMENTATIONKEY__",
+        extensioncontext: "WorkItemDetailsWidget",
+        disableTelemetry: "true",
+        disableAjaxTracking: "__disableAjaxTracking__",
+        enableDebug: "__enableDebug__"
+    };
+});
