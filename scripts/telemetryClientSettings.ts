cd5986396cc3209d40801fbd1c9e1ecebc94d362
chad2wick <chad2wick@users.noreply.github.com>
2020-01-06 20:38:30 -0500

    updated telemetry key for dev

--- scripts/telemetryClientSettings.ts
+++ scripts/telemetryClientSettings.ts
@@ -13,7 +13,7 @@
 import * as tc from "telemetryclient-team-services-extension";
 
 export const settings: tc.TelemetryClientSettings = {
-    key: "46a2ff3e-7764-437d-80dc-76d602287f48",
+    key: "468c8ddd-1f71-49f5-9b75-453f5c79c078",
     extensioncontext: "WorkItemPropertyWidget",
     disableTelemetry: "false",
     disableAjaxTracking: "true",
