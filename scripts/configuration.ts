d15392fd24a00ac174075e28d86d50458dd5415e
chad2wick <chad2wick@users.noreply.github.com>
2020-02-01 09:37:56 -0500

    fix bug so you can unselect a dropdown. prefix droplist of properties with a blank item.

--- scripts/configuration.ts
+++ scripts/configuration.ts
@@ -324,6 +324,7 @@ export class Configuration {
 
                 return 0;
             });
+            sortedFields.unshift("");
             deferred.resolve(sortedFields);
         });
 
@@ -395,7 +396,7 @@ export class Configuration {
     }
 
     public getCustomSettings() {
-        logger("getCustomSettings", "wiId: " + $("#wiid").val() + "; wipropertyname: " + this.$wipropertyname.val() + "; wicolorpropertyname: " + this.$wicolorpropertyname.val() + "; enableTelemetry: " + this.$enableTelemetry.is(":checked") + "; enableDebug: " + this.$enableDebug.is(":checked"));
+        logger("getCustomSettings", "wiId: " + $("#wiid").val() + "; wipropertyname: " + this.$wipropertyname.val() + "; wicolorpropertyname: " + this.$wicolorpropertyname.val() + "; dateFormat: " + this.$dateFormat + "; enableTelemetry: " + this.$enableTelemetry.is(":checked") + "; enableDebug: " + this.$enableDebug.is(":checked"));
         let result = { data: JSON.stringify(<ISettings>{
             wiId: $("#wiid").val(),
             wiPropertyName: $("#wipropertyname").val(),
